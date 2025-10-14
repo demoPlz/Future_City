4eb7102eefdf28437aa66e8cbc96bb417ac38790
Carton Liu <1435742009@qq.com>
2025-10-13 20:50:43 -0500

    import yushen Shepherd model in visionOS template

---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Building : MonoBehaviour
+{
+    public List<Floor> floors;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
